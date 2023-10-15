172783bb1cf4638a39c124739c8979dea24d9291
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-15 20:25:49 +0530

    context_processors list to show cart size

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cart.apps.CartConfig'
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -65,6 +65,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
