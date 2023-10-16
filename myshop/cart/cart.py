35d36355fc85efab4923d9c0b8106544f797a40f
Chiranjeevi <nchiranjeevi987@gmail.com>
2023-10-16 10:42:41 +0530

    context processor to show number of cart items

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -9,10 +9,8 @@ class Cart:
         if not cart:
             cart = self.session[settings.CART_SESSION_ID]={}
         self.cart = cart
-        print(self.cart)
 
     def add(self, product, quantity = 1, override_quantity =False):
-        print('hey')
         product_id = str(product.id)
         if product_id not in self.cart:
             self.cart[product_id]={
@@ -47,10 +45,12 @@ class Cart:
             item['total_price']=item['price']*item['quantity']
             yield item
 
-        def __len__(self):
-            return sum(item['quantity'] for item in self.cart.values())
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
 
-        def clear(self):
-            del self.session[settings.CART_SESSION_ID]
-            self.save()
+    def clear(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    
         
--- myshop/cart/context_processors.py
+++ myshop/cart/context_processors.py
@@ -1,4 +1,8 @@
 from .cart import Cart
 
 def cart(request):
+    cart = Cart(request)
+    cart_items = list(cart)  # Convert the cart items to a list
+    print(cart_items)  # Print the items in the cart
+    cart_length = len(cart)
     return {'cart': Cart(request)}
